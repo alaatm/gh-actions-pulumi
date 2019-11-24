a1357efca9926d3a7f6721fe77f32d701284ee7f
Alaa Masoud <alaa.masoud@live.com>
2019-11-24 18:57:03 +0300

    Update title Test package redeploy

--- src/App/ClientApp/src/Container.tsx
+++ src/App/ClientApp/src/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = (props: React.Props<{}>) => (
     <div className="container">
-        <h1 className="text-center">Github Actions Test Drive</h1>
+        <h1 className="text-center">Github Actions with Pulumi</h1>
         <hr />
         {props.children}
         <hr />
